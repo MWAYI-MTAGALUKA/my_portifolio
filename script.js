d85a2f2a5acd0cde97f3ce5c8f6f20b584b29da4
MWAI-MTAGALUKA <mwaimtagalyka@gmail.com>
2023-05-30 08:32:17 +0100

    update

---
+++ script.js
@@ -0,0 +1,24 @@
+
+            var i=0;
+            var images =[];
+            var time = 3000;
+            
+            images[0]='first.png';
+            images[1]='second.png';
+            images[2]='third.png';
+      
+            function changeImg(){
+              document.slide.src=images[i];
+      
+              if(i < images.length - 1){
+                  i++;
+              }else {
+                  i=0;
+              }
+      
+              setTimeout("changeImg()",time);
+            }
+      
+            windows.onload=changeImg;
+    
+          
